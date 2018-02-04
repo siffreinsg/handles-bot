a9f97d535540f42176182aa014ead10e253af774
Siffrein Sigy <siffr.hdesigy@gmail.com>
2018-02-04 12:37:26 +0100

    Help message for sudo command

--- src/app/commands/Dev/sudo.ts
+++ src/app/commands/Dev/sudo.ts
@@ -24,18 +24,22 @@ export default class Sudo extends Command {
         context.delete()
         switch ('' + args.get(0)) {
             case 'getrolesid':
-                let toSend = 'Available roles in the server ' + member.guild.name + ': \n\n'
-                member.guild.roles.array().forEach((role, index) => {
-                    toSend += '   - ' + role.name + ' | ID= ' + role.id + ' \n'
-                })
-                context.reply(replaceAll(toSend, '@everyone', 'everyone'), { split: true })
+                (async function () {
+                    let toSend = 'Available roles in the server ' + member.guild.name + ': \n\n'
+                    await member.guild.roles.array().forEach((role, index) => {
+                        toSend += '   - ' + role.name + ' | ID= ' + role.id + ' \n'
+                    })
+                    context.reply(replaceAll(toSend, '@everyone', 'everyone'), { split: true })
+                })()
                 break
             case 'joinrole':
-                let asked = context.message.content.toString().split(' ')[2]
-                if (!asked) return context.replyError('badArgs')
-                if (!member.guild.roles.has(asked)) return context.reply('ERROR: This role doesn\'t exist in this server !')
-                if (member.roles.has(asked)) return context.reply('ERROR: You already have this role !')
-                member.addRole(asked).then(member => context.reply('SUCCESS: You now have the role !')).catch(err => context.replyError())
+                (function () {
+                    let asked = context.message.content.toString().split(' ')[2]
+                    if (!asked) return context.replyError('badArgs')
+                    if (!member.guild.roles.has(asked)) return context.reply('ERROR: This role doesn\'t exist in this server !')
+                    if (member.roles.has(asked)) return context.reply('ERROR: You already have this role !')
+                    member.addRole(asked).then(member => context.reply('SUCCESS: You now have the role !')).catch(err => context.replyError())
+                })()
                 break
             case 'halt':
                 (async function () {
@@ -44,15 +48,33 @@ export default class Sudo extends Command {
                 })()
                 break
             case 'error':
-                context.replyError('' + args.get(1), '' + args.getProp('title'), '' + args.getProp('desc'))
+                (async function () {
+                    let data = args.getAll(),
+                        type = data.splice(0, 1)
+                    data = data.join(' ').split('|')
+                    context.replyError('' + type, '' + data[0], '' + data[1])
+                })()
                 break
             case 'setactivity':
-                let data = args.getAll()
-                data.splice(0, 1)
+                (async function () {
+                    let data = args.getAll()
+                    data.splice(0, 1)
+                    let activity = args.get(1) ? data.join(' ') : app.config.activity.replace('{prefix}', app.config.prefix)
 
-                let activity = args.get(1) ? data.join(' ') : app.config.activity.replace('{prefix}', app.config.prefix)
-                app.client.user.setActivity(activity)
-                context.reply(context.translate('/commands/setactivity'))
+                    await app.client.user.setActivity(activity)
+                    context.reply(context.translate('/commands/setactivity'))
+                })()
+                break
+            case 'help':
+                context.executor.send(
+                    'You need the SUPER_ADMIN permission to execute this command !\n\n' +
+                    '   - `sudo getrolesid`: *Get ids of availables roles in the server.*\n' +
+                    '   - `sudo joinrole <role id>`: *Get a role in a server.*\n' +
+                    '   - `sudo halt`: *Shutdown the bot.*\n' +
+                    '   - `sudo error [type] [title] | [desc]`: *Generate an error* **(Notice the | separating the title and the description)**.\n' +
+                    '   - `sudo setactivity [new activity]`: *Change or reset the bot\'s activity.*\n' +
+                    '   - `sudo help`: *Display this message.*'
+                )
                 break
             default:
                 return context.replyError('badArgs')
