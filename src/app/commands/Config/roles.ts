ac5f3c077f4d5e4b2910a805f0edb2437029ea51
Siffrein Sigy <siffr.hdesigy@gmail.com>
2018-03-16 14:51:49 +0100

    Added age check for roles

--- src/app/commands/Config/roles.ts
+++ src/app/commands/Config/roles.ts
@@ -17,7 +17,10 @@ export default class Roles extends Command {
     ]
     props: {} = {
         '--private': Boolean,
-        '-p': '--private'
+        '-p': '--private',
+        '--restrict': Boolean,
+        '-r': '--restrict',
+        '--nsfw': '--restrict'
     }
     allowDM: boolean = false
     aliases: string[] = ['role', 'channels']
@@ -33,7 +36,11 @@ export default class Roles extends Command {
                         toSend = ''
                     if (roles.length > 0) {
                         await roles.forEach(role => {
-                            if (!role.isPrivate) toSend += context.translate('/commands/roles/roleFormat', { name: role.name, channel: '<#' + role.channel + '>' }) + '\n'
+                            if (!role.isPrivate) toSend += context.translate('/commands/roles/roleFormat', {
+                                name: role.name,
+                                channel: '<#' + role.channel + '>',
+                                restricted: (role.isAgeRestricted ? context.translate('/commands/roles/roleRestricted') : '')
+                            }) + '\n'
                         })
                         context.reply(context.translate('/commands/roles/availableRoles') + '\n' + toSend, { split: true })
                     } else {
@@ -49,7 +56,8 @@ export default class Roles extends Command {
                         let name: any = args.getAll(),
                             channel = context.message.mentions.channels.first(),
                             role = context.server.roles.find('name', name),
-                            isPrivate: boolean = false
+                            isPrivate: boolean = false,
+                            isAgeRestricted: boolean = false
                         name.splice(0, 1)
                         name = name.join(' ')
                         if (channel) name.splice(0, -1)
@@ -59,6 +67,7 @@ export default class Roles extends Command {
                         if (roles.find({ name: name }).value()) return context.replyError('custom', context.translate('/commands/roles/roleExists'), context.translate('/commands/roles/useOtherName'))
 
                         if (args.getProp('--private')) isPrivate = true
+                        if (args.getProp('--restrict')) isAgeRestricted = true
                         if (!role) context.server.createRole({ name: name, color: 'RANDOM', mentionable: false }).then((rol: any) => {
                             if (!channel || channel.type !== 'text') context.server.createChannel(replaceAll(name, ' ', '-'), 'text').then((chan: any) => {
                                 next(rol, chan)
@@ -70,7 +79,7 @@ export default class Roles extends Command {
                         async function next(rol, chan) {
                             chan.overwritePermissions(rol, { 'READ_MESSAGES': true })
                             chan.overwritePermissions(context.server.defaultRole, { 'READ_MESSAGES': false })
-                            roles.push({ roleid: rol.id, name: name, channel: chan.id, isPrivate }).write()
+                            roles.push({ roleid: rol.id, name, channel: chan.id, isPrivate, isAgeRestricted }).write()
                             msg.edit(context.translate('/commands/roles/created', { name, channel: '<#' + chan.id + '>' }))
                         }
                     })
@@ -125,13 +134,24 @@ export default class Roles extends Command {
                 (async function () {
                     let roles = app.db.createIfNotExists(app.db.getConfig(context.server.id), 'roles', [])
                     let name: any = args.getAll(),
-                        member = context.server.member(context.executor)
+                        member = context.server.member(context.executor),
+                        age = app.db.createIfNotExists(app.db.getUser(context.server.id, member.id), 'profile', {}).value().birthdate
                     name.splice(0, 1)
                     name = name.join(' ')
 
                     let role = await roles.find({ name: name }).value()
                     if (!role || !context.server.roles.has(role.roleid)) return context.replyError('custom', context.translate('/commands/roles/noRole'), context.translate('/commands/roles/doesntExists'))
                     if (role.isPrivate) return context.replyError('custom', context.translate('/commands/roles/privateRole'), context.translate('/commands/roles/cantAccess'))
+                    if (age && role.isAgeRestricted) {
+                        let now = new Date(),
+                            old = new Date(age),
+                            diff = now.getFullYear() - old.getFullYear()
+                        if (old.getMonth() > now.getMonth()) diff--
+                        else if (old.getMonth() === now.getMonth()) {
+                            if (old.getDate() > now.getDate()) diff--
+                        }
+                        if (diff < 18) return context.replyError('custom', context.translate('/commands/roles/restricted'), context.translate('/commands/roles/invalidAge'))
+                    }
                     if (member.roles.has(role.roleid)) return context.replyError('custom', context.translate('/errors/error'), context.translate('/commands/roles/alreadyHave'))
 
                     member.addRole(role.roleid).then(member => context.reply(context.translate('/commands/roles/roleGived', { name }))).catch(err => context.replyError())
